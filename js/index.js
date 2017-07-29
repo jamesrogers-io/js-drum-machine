b06813eb3985c7139071e66f139d8b8ecdca011a
James Rogers <contact@jamesrogers.io>
2017-07-28 22:47:03 -0700

    Made the buttons more responsive to Clicks

--- js/index.js
+++ js/index.js
@@ -42,125 +42,149 @@ const padNine = document.getElementById('pad-nine');
 
 // PAD 1: CLICK FUNCTION
 const playPadOne = function() {
+  const padOne = document.getElementById('pad-one');
   const wavOne = document.querySelector('audio[data-key="103"]');
   const greenLight = document.getElementById('green-light');
 
   greenLight.classList.add('green');
+  padOne.classList.remove('z-depth-1');
   wavOne.currentTime = 0;
   wavOne.play();
 
   setTimeout(function() {
     greenLight.classList.remove('green');
+    padOne.classList.add('z-depth-1');
+
   }, 100);
 };
 // PAD 2: CLICK FUNCTION
 const playPadTwo = function() {
+  const padTwo = document.getElementById('pad-two');
   const wavTwo = document.querySelector('audio[data-key="104"]');
   const greenLight = document.getElementById('green-light');
 
   greenLight.classList.add('green');
+  padTwo.classList.remove('z-depth-1');
   wavTwo.currentTime = 0;
   wavTwo.play();
 
   setTimeout(function() {
     greenLight.classList.remove('green');
+    padTwo.classList.add('z-depth-1');
   }, 100);
 };
 // PAD 3: CLICK FUNCTION
 const playPadThree = function() {
+  const padThree = document.getElementById('pad-three');
   const wavThree = document.querySelector('audio[data-key="105"]');
   const greenLight = document.getElementById('green-light');
 
   greenLight.classList.add('green');
+  padThree.classList.remove('z-depth-1');
   wavThree.currentTime = 0;
   wavThree.play();
 
   setTimeout(function() {
     greenLight.classList.remove('green');
+    padThree.classList.add('z-depth-1');
   }, 100);
 };
 // PAD 4: CLICK FUNCTION
 const playPadFour = function() {
+  const padFour = document.getElementById('pad-four');
   const wavFour = document.querySelector('audio[data-key="100"]');
   const greenLight = document.getElementById('green-light');
 
   greenLight.classList.add('green');
+  padFour.classList.remove('z-depth-1');
   wavFour.currentTime = 0;
   wavFour.play();
 
   setTimeout(function() {
     greenLight.classList.remove('green');
+    padFour.classList.add('z-depth-1');
   }, 100);
 };
 // PAD 5: CLICK FUNCTION
 const playPadFive = function() {
+  const padFive = document.getElementById('pad-five');
   const wavFive = document.querySelector('audio[data-key="101"]');
   const greenLight = document.getElementById('green-light');
 
   greenLight.classList.add('green');
+  padFive.classList.remove('z-depth-1');
   wavFive.currentTime = 0;
   wavFive.play();
 
   setTimeout(function() {
     greenLight.classList.remove('green');
+    padFive.classList.add('z-depth-1');
   }, 100);
 };
 // PAD 6: CLICK FUNCTION
 const playPadSix = function() {
+  const padSix = document.getElementById('pad-six');
   const wavSix = document.querySelector('audio[data-key="102"]');
   const greenLight = document.getElementById('green-light');
 
   greenLight.classList.add('green');
+  padSix.classList.remove('z-depth-1');
   wavSix.currentTime = 0;
   wavSix.play();
 
   setTimeout(function() {
     greenLight.classList.remove('green');
+    padSix.classList.add('z-depth-1');
   }, 100);
 };
 // PAD 7: CLICK FUNCTION
 const playPadSeven = function() {
+  const padSeven = document.getElementById('pad-seven');
   const wavSeven = document.querySelector('audio[data-key="97"]');
   const greenLight = document.getElementById('green-light');
 
   greenLight.classList.add('green');
+  padSeven.classList.remove('z-depth-1');
   wavSeven.currentTime = 0;
   wavSeven.play();
 
   setTimeout(function() {
     greenLight.classList.remove('green');
+    padSeven.classList.add('z-depth-1');
   }, 100);
 };
 // PAD 8: CLICK FUNCTION
 const playPadEight = function() {
+  const padEight = document.getElementById('pad-eight');
   const wavEight = document.querySelector('audio[data-key="98"]');
   const greenLight = document.getElementById('green-light');
 
   greenLight.classList.add('green');
+  padEight.classList.remove('z-depth-1');
   wavEight.currentTime = 0;
   wavEight.play();
 
   setTimeout(function() {
     greenLight.classList.remove('green');
+    padEight.classList.add('z-depth-1');
   }, 100);
 };
 // PAD 9: CLICK FUNCTION
 const playPadNine = function() {
+  const padNine = document.getElementById('pad-nine');
   const wavNine = document.querySelector('audio[data-key="99"]');
   const greenLight = document.getElementById('green-light');
 
   greenLight.classList.add('green');
+  padNine.classList.remove('z-depth-1');
   wavNine.currentTime = 0;
   wavNine.play();
 
   setTimeout(function() {
     greenLight.classList.remove('green');
+    padNine.classList.add('z-depth-1');
   }, 100);
 };
-
-
-
-
 
 
 
